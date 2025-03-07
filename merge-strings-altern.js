e9662cf859052f06faf9599b2a12b40dcba1d712
Larisa Capatina <117665726+pianist-in-tech@users.noreply.github.com>
2025-03-07 10:59:52 -0700

    Update merge-strings-altern.js

--- merge-strings-altern.js
+++ merge-strings-altern.js
@@ -11,3 +11,18 @@
 // word2:    p   q   r
 // merged: a p b q c r
 
+function mergeAlternately(word1, word2){
+    let mergedStr = '';
+    const longestStrLen = Math.max(word1.length, word2.length)
+    //iterate through both strings
+    for (let i = 0; i < longestStrLen; i++){
+        if (i < word1.length) {
+            mergedStr += word1[i];
+        }
+        if (i < word2.length) {
+            mergedStr += word2[i];
+        }
+        }
+    return mergedStr;
+}
+//The time complexity of your solution is O(n), where n is the length of the longest string (Math.max(word1.length, word2.length)).
